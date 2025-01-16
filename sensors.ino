9d5a37cf7c20d921ca711cb153bed8d2e293888e
Lnola <nola.luka@gmail.com>
2025-01-16 18:06:03 +0100

    💡 Comment out the temp digital code

--- sensors.ino
+++ sensors.ino
@@ -132,25 +132,25 @@
 // *** FIRE ***
 
 // *** TEMP DIGITAL ***
-//----Begin Code (copy from here)----
-//Includes:
-#include <OneWire.h> //includes the functions needed to communicate with the 1-wire protocol
-#include <DallasTemperature.h> //includes conversion functions from F to C etc, as well as the actual code to talk to the sensor.
-//Variables:
-OneWire oneWire(D7); //Set the One Wire Data bus on pin 7.
-DallasTemperature sensor(&oneWire); //This is passing a reference to the sensor object. This isnt commonly needed. 
+// //----Begin Code (copy from here)----
+// //Includes:
+// #include <OneWire.h> //includes the functions needed to communicate with the 1-wire protocol
+// #include <DallasTemperature.h> //includes conversion functions from F to C etc, as well as the actual code to talk to the sensor.
+// //Variables:
+// OneWire oneWire(D7); //Set the One Wire Data bus on pin 7.
+// DallasTemperature sensor(&oneWire); //This is passing a reference to the sensor object. This isnt commonly needed.
 
-void setup() {
- // put your setup code here, to run once:
-Serial.begin(9600);
-Serial.println("Communicating with the temperature sensor via 1-wire protocol. \nFetching data...");
-sensor.begin(); //Begin talking to the sensor
-}
-void loop() {
- // put your main code here, to run repeatedly:
-sensor.requestTemperatures(); //Request temperatures from the sensor
-Serial.print("Temp: "); //print "Temp: "
-Serial.print(sensor.getTempCByIndex(0)); //populate the temperature reading.
-Serial.println("c"); //print "Temp: "
-}
+// void setup() {
+//  // put your setup code here, to run once:
+// Serial.begin(9600);
+// Serial.println("Communicating with the temperature sensor via 1-wire protocol. \nFetching data...");
+// sensor.begin(); //Begin talking to the sensor
+// }
+// void loop() {
+//  // put your main code here, to run repeatedly:
+// sensor.requestTemperatures(); //Request temperatures from the sensor
+// Serial.print("Temp: "); //print "Temp: "
+// Serial.print(sensor.getTempCByIndex(0)); //populate the temperature reading.
+// Serial.println("c"); //print "Temp: "
+// }
 // *** TEMP DIGITAL ***
