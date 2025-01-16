9c52e3fa9ad901b1071375d8ed64b6b8a5c5a861
Lnola <nola.luka@gmail.com>
2025-01-16 18:42:01 +0100

    ✨ Add temperature and humidity reading with the DHT sensor

--- test.ino
+++ test.ino
@@ -1,8 +1,30 @@
 #include <Ticker.h>
+#include "dhtnew.h"
+
+#define DHTPIN D7
+
+DHTNEW dht(DHTPIN);
 
 const int photoresistorPin = A0;
 
+Ticker dhtTicker;
 Ticker photoresistorTicker;
+
+
+void read_dht() {
+  if (dht.read()) {
+    float temperature = dht.getTemperature();
+    float humidity = dht.getHumidity();
+
+    Serial.print("[DHT]: Temperature: ");
+    Serial.print(temperature);
+    Serial.print("°C, Humidity: ");
+    Serial.print(humidity);
+    Serial.println("%");
+  } else {
+    Serial.println("[DHT]: Sensor not ready or failed to read");
+  }
+}
 
 void read_photoresistor() {
   int val = analogRead(photoresistorPin);
@@ -12,6 +34,8 @@ void read_photoresistor() {
 
 void setup() {
   Serial.begin(9600);
+
+  dhtTicker.attach(1, read_dht);
   photoresistorTicker.attach(0.1, read_photoresistor);
 }
 
