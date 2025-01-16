8cb38bfd35bad10fe73e65be70b2a34d4a419ccb
Lnola <nola.luka@gmail.com>
2025-01-16 22:24:17 +0100

    🔨 Add separate topics to listener.sh

--- listener.sh
+++ listener.sh
@@ -1,2 +1,6 @@
 #!/bin/zsh
 mosquitto_sub -h broker.hivemq.com -p 1883 -t test/topic -d
+mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/temperature -d
+mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/humidity -d
+mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/light-level -d
+mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/flame -d
