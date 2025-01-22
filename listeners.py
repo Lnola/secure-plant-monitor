c0b59cbc336af0c5bd48d8aff88526e9a057bae6
Lnola <nola.luka@gmail.com>
2025-01-22 21:50:15 +0100

    ✨ Make the tabs open in split views

--- listeners.py
+++ listeners.py
@@ -9,35 +9,32 @@ commands = [
     """mosquitto_sub -h 188.245.235.247 --cafile ~/Downloads/ca.crt --cert ~/Downloads/client.crt --key ~/Downloads/client.key -p 8883 -t 'lnola/sensor/flame' """,
 ]
 
-def run_iterm_commands(commands):
-    script = """
+def open_tab_with_horizontal_splits_and_run_commands(commands):
+    applescript = """
     tell application "iTerm2"
         activate
-        set newWindow to (create window with default profile)
+        tell current window
+            create tab with default profile
+            tell current session
+                split horizontally with default profile
+                split horizontally with default profile
+                split horizontally with default profile
+            end tell
     """
+    # Run the commands in the respective panes
     for index, command in enumerate(commands):
-        if index == 0:
-            # First command runs in the initial tab
-            script += f"""
-            tell current session of newWindow
+        applescript += f"""
+            tell session {index + 1} of current tab
                 write text "{command.replace('"', '\\"')}"
             end tell
-            """
-        else:
-            # Subsequent commands open in new tabs
-            script += f"""
-            tell newWindow
-                create tab with default profile
-                tell current session
-                    write text "{command.replace('"', '\\"')}"
-                end tell
-            end tell
-            """
-    script += "end tell"
+        """
+    applescript += """
+        end tell
+    end tell
+    """
 
     # Execute the AppleScript
-    subprocess.run(["osascript", "-e", script])
+    subprocess.run(["osascript", "-e", applescript])
 
-
-# Run the script
-run_iterm_commands(commands)
+# Run the function
+open_tab_with_horizontal_splits_and_run_commands(commands)
