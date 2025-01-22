5ad3c77a51abb981c0442f9e0c21bb4b5a00628b
Lnola <nola.luka@gmail.com>
2025-01-22 21:27:13 +0100

    ⚰️ Remove the listener.sh script

--- listener.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-mosquitto_sub -h broker.hivemq.com -p 1883 -t test/topic -d
-mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/temperature -d
-mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/humidity -d
-mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/light-level -d
-mosquitto_sub -h broker.hivemq.com -p 1883 -t lnola/sensor/flame -d
